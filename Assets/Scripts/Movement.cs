cd84d2232810edc8c98fc6a7d342ba73bc10cc8e
Paul Hielscher <65562617+Vanhoras@users.noreply.github.com>
2025-01-26 01:08:50 +0100

    Added bubbles

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -116,11 +116,11 @@ public class Movement : MonoBehaviour
             inputVector = worldMousePosition - (Vector2)transform.position;
         }
 
-        if (inputVector.x > 0)
+        if (inputVector.x > 0.1)
         {
             direction = DirectionE.Right;
         }
-        else if (inputVector.x < 0)
+        else if (inputVector.x < -0.1)
         {
             direction = DirectionE.Left;
         }
